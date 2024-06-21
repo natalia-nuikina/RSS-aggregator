0b619560921b1ce9e4a3db995e743fd85d6dc49d
Наталья Нуйкина <me@nuikina.ru>
2024-06-21 16:35:26 +0300

    Проверка 1

--- src/app.js
+++ src/app.js
@@ -39,93 +39,91 @@ const state = {
 };
 
 export default () => {
-
-
   const defaultLanguage = 'ru';
   i18next.init({
     lng: defaultLanguage,
     debug: false,
     resources,
   })
-  .then(() => {
-    const watchedState = watch(elements, i18next, state);
+    .then(() => {
+      const watchedState = watch(elements, i18next, state);
 
-    const getNewPosts = (feeds) => {
-      feeds.forEach((feed) => {
-        axios.get(feed.url)
+      const getNewPosts = (feeds) => {
+        feeds.forEach((feed) => {
+          axios.get(feed.url)
+            .then((response) => {
+              const [, posts] = parser(response.data.contents);
+              const filterPost = (post) => {
+                if (post.timeOfPost > feed.lastUpdate) {
+                  return true;
+                }
+                return false;
+              };
+
+              const newPosts = posts.filter(filterPost);
+
+              newPosts.map((post) => {
+                post.id = uniqueId();
+                post.feedId = feed.id;
+                watchedState.posts.push(post);
+                feed.lastUpdate = post.timeOfPost;
+                return post;
+              });
+              watchedState.form.status = 'finished';
+              setTimeout(() => getNewPosts(watchedState.feeds), 5000);
+            })
+            .catch((err) => {
+              watchedState.form.status = 'failed';
+              watchedState.form.valid = false;
+              watchedState.form.error = (axios.isAxiosError(err)) ? 'networkError' : err.message;
+            });
+        });
+      };
+
+      const getFeedAndPosts = (url) => {
+        axios.get(makeUrl(url))
           .then((response) => {
-            const [, posts] = parser(response.data.contents);
-            const filterPost = (post) => {
-              if (post.timeOfPost > feed.lastUpdate) {
-                return true;
-              }
-              return false;
-            };
-
-            const newPosts = posts.filter(filterPost);
-
-            newPosts.map((post) => {
+            const [feed, posts] = parser(response.data.contents);
+            feed.url = makeUrl(url);
+            feed.lastUpdate = posts[posts.length - 1].timeOfPost;
+            feed.id = uniqueId();
+            posts.forEach((post) => {
               post.id = uniqueId();
               post.feedId = feed.id;
-              watchedState.posts.push(post);
-              feed.lastUpdate = post.timeOfPost;
-              return post;
             });
             watchedState.form.status = 'finished';
-            setTimeout(() => getNewPosts(watchedState.feeds), 5000);
+            watchedState.feeds.push(feed);
+            watchedState.posts = posts;
+            getNewPosts(watchedState.feeds);
           })
           .catch((err) => {
-            watchedState.form.status = 'failed';
+            watchedState.form.error = (err.isAxiosError) ? 'networkError' : err.message;
             watchedState.form.valid = false;
-            watchedState.form.error = (axios.isAxiosError(err)) ? 'networkError' : err.message;
+            watchedState.form.status = 'failed';
           });
-      });
-    };
-  
-    const getFeedAndPosts = (url) => {
-      axios.get(makeUrl(url))
-        .then((response) => {
-          const [feed, posts] = parser(response.data.contents);
-          feed.url = makeUrl(url);
-          feed.lastUpdate = posts[posts.length - 1].timeOfPost;
-          feed.id = uniqueId();
-          posts.forEach((post) => {
-            post.id = uniqueId();
-            post.feedId = feed.id;
+      };
+
+      elements.form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        watchedState.form.status = 'sending';
+        const formData = new FormData(elements.form);
+        const url = formData.get('url');
+        const schema = yup.object({
+          website: yup.string().url().notOneOf(watchedState.form.watchUrl),
+        });
+        schema.validate({ website: url })
+          .then(() => {
+            watchedState.form.valid = true;
+            watchedState.form.error = null;
+            getFeedAndPosts(url);
+            watchedState.form.watchUrl.push(url);
+          })
+          .catch((err) => {
+            watchedState.form.error = err.type;
+            watchedState.form.valid = false;
+            watchedState.form.status = 'failed';
           });
-          watchedState.form.status = 'finished';
-          watchedState.feeds.push(feed);
-          watchedState.posts = posts;
-          getNewPosts(watchedState.feeds);
-        })
-        .catch((err) => {
-          watchedState.form.error = (err.isAxiosError) ? 'networkError' : err.message;
-          watchedState.form.valid = false;
-          watchedState.form.status = 'failed';
-        });
-    };
-  
-    elements.form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      watchedState.form.status = 'sending';
-      const formData = new FormData(elements.form);
-      const url = formData.get('url');
-      const schema = yup.object({
-        website: yup.string().url().notOneOf(watchedState.form.watchUrl),
+        watch(elements, i18next, watchedState);
       });
-      schema.validate({ website: url })
-        .then(() => {
-          watchedState.form.valid = true;
-          watchedState.form.error = null;
-          getFeedAndPosts(url);
-          watchedState.form.watchUrl.push(url);
-        })
-        .catch((err) => {
-          watchedState.form.error = err.type;
-          watchedState.form.valid = false;
-          watchedState.form.status = 'failed';
-        });
-      watch(elements, i18next, watchedState);
     });
-  })
 };
