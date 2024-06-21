df8ba8a40d3133a0a65ff5b561feba9ae7af6031
Наталья Нуйкина <me@nuikina.ru>
2024-06-21 16:07:42 +0300

    Проверка 1

--- src/app.js
+++ src/app.js
@@ -7,129 +7,130 @@ import resources from './languages/ru.js';
 import makeUrl from './helpers.js';
 import parser from './parser.js';
 
+const elements = {
+  title: document.querySelector('h1'),
+  subtitle: document.querySelector('.lead'),
+  label: document.querySelector('[for="url-input"]'),
+  add: document.querySelector('[type="submit"]'),
+  example: document.querySelector('.example'),
+  fullArticle: document.querySelector('.full-article'),
+  buttonClose: document.querySelector('.btn-secondary'),
+  form: document.querySelector('form'),
+  input: document.querySelector('input'),
+  feedback: document.querySelector('.feedback'),
+  feeds: document.querySelector('.feeds'),
+  posts: document.querySelector('.posts'),
+  modal: document.querySelector('#modal'),
+  modalTitle: document.querySelector('.modal-title'),
+  modalBody: document.querySelector('.modal-body'),
+  modalArticle: document.querySelector('.full-article'),
+};
+
+const state = {
+  form: {
+    status: 'filling',
+    valid: true,
+    error: null,
+    watchUrl: [],
+  },
+  feeds: [],
+  posts: [],
+  ulStateOpened: [],
+};
+
 export default () => {
+  const schema = yup.object({
+    website: yup.string().url().notOneOf(state.form.watchUrl),
+  });
+
   const defaultLanguage = 'ru';
   i18next.init({
     lng: defaultLanguage,
     debug: false,
     resources,
-  });
+  })
+  .then(() => {
+    const watchedState = watch(elements, i18next, state);
 
-  const elements = {
-    title: document.querySelector('h1'),
-    subtitle: document.querySelector('.lead'),
-    label: document.querySelector('[for="url-input"]'),
-    add: document.querySelector('[type="submit"]'),
-    example: document.querySelector('.example'),
-    fullArticle: document.querySelector('.full-article'),
-    buttonClose: document.querySelector('.btn-secondary'),
-    form: document.querySelector('form'),
-    input: document.querySelector('input'),
-    feedback: document.querySelector('.feedback'),
-    feeds: document.querySelector('.feeds'),
-    posts: document.querySelector('.posts'),
-    modal: document.querySelector('#modal'),
-    modalTitle: document.querySelector('.modal-title'),
-    modalBody: document.querySelector('.modal-body'),
-    modalArticle: document.querySelector('.full-article'),
-  };
+    const getNewPosts = (feeds) => {
+      feeds.forEach((feed) => {
+        axios.get(feed.url)
+          .then((response) => {
+            const [, posts] = parser(response.data.contents);
+            const filterPost = (post) => {
+              if (post.timeOfPost > feed.lastUpdate) {
+                return true;
+              }
+              return false;
+            };
 
-  const state = {
-    form: {
-      status: 'filling',
-      valid: true,
-      error: null,
-      watchUrl: [],
-    },
-    feeds: [],
-    posts: [],
-    ulStateOpened: [],
-  };
+            const newPosts = posts.filter(filterPost);
 
-  const schema = yup.object({
-    website: yup.string().url().notOneOf(state.form.watchUrl),
-  });
-
-  const watchedState = watch(elements, i18next, state);
-
-  const getNewPosts = (feeds) => {
-    feeds.forEach((feed) => {
-      axios.get(feed.url)
+            newPosts.map((post) => {
+              post.id = uniqueId();
+              post.feedId = feed.id;
+              watchedState.posts.push(post);
+              feed.lastUpdate = post.timeOfPost;
+              return post;
+            });
+            watchedState.form.status = 'finished';
+            setTimeout(() => getNewPosts(watchedState.feeds), 5000);
+          })
+          .catch((err) => {
+            watchedState.form.status = 'failed';
+            watchedState.form.valid = false;
+            watchedState.form.error = (axios.isAxiosError(err)) ? 'networkError' : err.message;
+          });
+      });
+    };
+  
+    const getFeedAndPosts = (url) => {
+      axios.get(makeUrl(url))
         .then((response) => {
-          const [, posts] = parser(response.data.contents);
-          const filterPost = (post) => {
-            if (post.timeOfPost > feed.lastUpdate) {
-              return true;
-            }
-            return false;
-          };
-
-          const newPosts = posts.filter(filterPost);
-
-          newPosts.map((post) => {
+          const [feed, posts] = parser(response.data.contents);
+          feed.url = makeUrl(url);
+          feed.lastUpdate = posts[posts.length - 1].timeOfPost;
+          feed.id = uniqueId();
+          posts.forEach((post) => {
             post.id = uniqueId();
             post.feedId = feed.id;
-            watchedState.posts.push(post);
-            feed.lastUpdate = post.timeOfPost;
-            return post;
           });
           watchedState.form.status = 'finished';
-          setTimeout(() => getNewPosts(watchedState.feeds), 5000);
+          watchedState.feeds.push(feed);
+          watchedState.posts = posts;
+          getNewPosts(watchedState.feeds);
         })
         .catch((err) => {
-          watchedState.form.status = 'failed';
+          watchedState.form.error = (err.isAxiosError) ? 'networkError' : err.message;
           watchedState.form.valid = false;
-          watchedState.form.error = (axios.isAxiosError(err)) ? 'networkError' : err.message;
+          watchedState.form.status = 'failed';
         });
+    };
+  
+    elements.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      watchedState.form.status = 'sending';
+      const formData = new FormData(elements.form);
+      const url = formData.get('url');
+      schema.validate({ website: url })
+        .then(() => {
+          watchedState.form.valid = true;
+          watchedState.form.error = null;
+          if (state.form.watchUrl.includes(url)) {
+            watchedState.form.error = 'duplicate';
+            watchedState.form.valid = false;
+            watchedState.form.status = 'failed';
+          } else {
+            getFeedAndPosts(url);
+            watchedState.form.watchUrl.push(url);
+          }
+        })
+        .catch((err) => {
+          watchedState.form.error = err.type;
+          watchedState.form.valid = false;
+          watchedState.form.status = 'failed';
+        });
+      watch(elements, i18next, watchedState);
     });
-  };
-
-  const getFeedAndPosts = (url) => {
-    axios.get(makeUrl(url))
-      .then((response) => {
-        const [feed, posts] = parser(response.data.contents);
-        feed.url = makeUrl(url);
-        feed.lastUpdate = posts[posts.length - 1].timeOfPost;
-        feed.id = uniqueId();
-        posts.forEach((post) => {
-          post.id = uniqueId();
-          post.feedId = feed.id;
-        });
-        watchedState.form.status = 'finished';
-        watchedState.feeds.push(feed);
-        watchedState.posts = posts;
-        getNewPosts(watchedState.feeds);
-      })
-      .catch((err) => {
-        watchedState.form.error = (err.isAxiosError) ? 'networkError' : err.message;
-        watchedState.form.valid = false;
-        watchedState.form.status = 'failed';
-      });
-  };
-
-  elements.form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    watchedState.form.status = 'sending';
-    const formData = new FormData(elements.form);
-    const url = formData.get('url');
-    schema.validate({ website: url })
-      .then(() => {
-        watchedState.form.valid = true;
-        watchedState.form.error = null;
-        if (state.form.watchUrl.includes(url)) {
-          watchedState.form.error = 'duplicate';
-          watchedState.form.valid = false;
-          watchedState.form.status = 'failed';
-        } else {
-          getFeedAndPosts(url);
-          watchedState.form.watchUrl.push(url);
-        }
-      })
-      .catch((err) => {
-        watchedState.form.error = err.type;
-        watchedState.form.valid = false;
-        watchedState.form.status = 'failed';
-      });
-    watch(elements, i18next, watchedState);
-  });
+  })
 };
--- src/view.js
+++ src/view.js
@@ -1,6 +1,144 @@
 import onChange from 'on-change';
 import { Modal } from 'bootstrap';
 
+const renderFeedback = (elements, i18n, watchedState) => {
+  elements.feedback.classList.add('text-danger');
+  switch (watchedState.form.error) {
+    case 'url':
+      elements.feedback.textContent = i18n.t('feedbacks.errors.invalid');
+      break;
+    case 'duplicate':
+      elements.feedback.textContent = i18n.t('feedbacks.errors.duplicate');
+      break;
+    case 'invalidRss':
+      elements.feedback.textContent = i18n.t('feedbacks.errors.invalidRss');
+      break;
+    case 'networkError':
+      elements.feedback.textContent = i18n.t('feedbacks.errors.networkError');
+      break;
+    default:
+      elements.feedback.textContent = i18n.t('feedbacks.valid');
+      elements.feedback.style.color = 'green';
+      elements.feedback.classList.remove('text-danger');
+      break;
+  }
+};
+
+const renderStatus = (elements, watchedState) => {
+  switch (watchedState.form.status) {
+    case 'finished':
+      elements.input.value = '';
+      elements.input.focus();
+      elements.add.removeAttribute('disabled');
+      break;
+    case 'sending':
+      elements.add.setAttribute('disabled', true);
+      break;
+    case 'failed':
+      elements.add.removeAttribute('disabled');
+      break;
+    default:
+      break;
+  }
+};
+const renderFeeds = (elements, i18n, watchedState) => {
+  elements.feeds.innerHTML = '';
+  const feedsCard = document.createElement('div');
+  feedsCard.classList.add('card', 'border-0');
+  elements.feeds.append(feedsCard);
+  const feedsBody = document.createElement('div');
+  feedsBody.classList.add('card-body');
+  feedsCard.append(feedsBody);
+  const feedsTitle = document.createElement('h2');
+  feedsTitle.classList.add('card-title', 'h4');
+  feedsTitle.textContent = i18n.t('feeds');
+  feedsBody.append(feedsTitle);
+  const ul = document.createElement('ul');
+  ul.classList.add('list-group', 'border-0', 'rounded-0');
+  feedsCard.append(ul);
+  watchedState.feeds.forEach((feed) => {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'border-0', 'border-end-0');
+    ul.append(li);
+    const h3 = document.createElement('h3');
+    h3.classList.add('h6', 'm-0');
+    h3.textContent = feed.text;
+    const p = document.createElement('p');
+    p.classList.add('m-0', 'small', 'text-black-50');
+    p.textContent = feed.description;
+    li.append(h3, p);
+  });
+};
+
+const renderPosts = (elements, i18n, watchedState) => {
+  elements.posts.innerHTML = '';
+  const postsCard = document.createElement('div');
+  postsCard.classList.add('card', 'border-0');
+  elements.posts.append(postsCard);
+  const postsBody = document.createElement('div');
+  postsBody.classList.add('card-body');
+  postsCard.append(postsBody);
+  const postsTitle = document.createElement('h2');
+  postsTitle.classList.add('card-title', 'h4');
+  postsTitle.textContent = i18n.t('posts');
+  postsBody.append(postsTitle);
+  const ul = document.createElement('ul');
+  ul.classList.add('list-group', 'border-0', 'rounded-0');
+  postsCard.append(ul);
+
+  watchedState.posts.forEach((post) => {
+    const {
+      id, text, link,
+    } = post;
+    const li = document.createElement('li');
+    const button = document.createElement('button');
+    const a = document.createElement('a');
+
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+    ul.prepend(li);
+
+    a.href = link;
+    a.textContent = text;
+    a.target = '_blank';
+    a.setAttribute('rel', 'noopener noreferrer');
+    a.setAttribute('data-post-id', id);
+    if (watchedState.ulStateOpened.includes(id)) {
+      a.classList.add('fw-normal');
+    } else {
+      a.classList.add('fw-bold');
+    }
+
+    button.type = 'button';
+    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    button.textContent = i18n.t('button');
+    button.setAttribute('data-post-id', id);
+    li.append(a, button);
+  });
+
+  ul.addEventListener('click', (e) => {
+    const button = e.target.closest('button');
+    const a = e.target.closest('a');
+
+    if (button) {
+      watchedState.ulStateOpened.push(button.dataset.postId);
+      const [targetPost] = watchedState.posts.filter((post) => post.id === button.dataset.postId);
+      const targetA = document.querySelector(`a[data-post-id="${button.dataset.postId}"]`);
+      targetA.classList.remove('fw-bold');
+      targetA.classList.add('fw-normal');
+
+      const modal = new Modal(elements.modal);
+      elements.modalTitle.textContent = targetPost.text;
+      elements.modalBody.textContent = targetPost.description;
+      elements.modalArticle.href = targetPost.link;
+      modal.show();
+    } else if (a) {
+      watchedState.ulStateOpened.push(a.dataset.postId);
+      a.classList.remove('fw-bold');
+      a.classList.add('fw-normal');
+    }
+  });
+};
+
 export default (details, i18next, state) => {
   const render = (elements, i18n) => {
     elements.title.textContent = i18n.t('title');
@@ -18,144 +156,6 @@ export default (details, i18next, state) => {
     } else {
       elements.input.classList.add('is-invalid');
     }
-  };
-
-  const renderFeedback = (elements, i18n, watchedState) => {
-    elements.feedback.classList.add('text-danger');
-    switch (watchedState.form.error) {
-      case 'url':
-        elements.feedback.textContent = i18n.t('feedbacks.errors.invalid');
-        break;
-      case 'duplicate':
-        elements.feedback.textContent = i18n.t('feedbacks.errors.duplicate');
-        break;
-      case 'invalidRss':
-        elements.feedback.textContent = i18n.t('feedbacks.errors.invalidRss');
-        break;
-      case 'networkError':
-        elements.feedback.textContent = i18n.t('feedbacks.errors.networkError');
-        break;
-      default:
-        elements.feedback.textContent = i18n.t('feedbacks.valid');
-        elements.feedback.style.color = 'green';
-        elements.feedback.classList.remove('text-danger');
-        break;
-    }
-  };
-
-  const renderStatus = (elements, watchedState) => {
-    switch (watchedState.form.status) {
-      case 'finished':
-        elements.input.value = '';
-        elements.input.focus();
-        elements.add.removeAttribute('disabled');
-        break;
-      case 'sending':
-        elements.add.setAttribute('disabled', true);
-        break;
-      case 'failed':
-        elements.add.removeAttribute('disabled');
-        break;
-      default:
-        break;
-    }
-  };
-  const renderFeeds = (elements, i18n, watchedState) => {
-    elements.feeds.innerHTML = '';
-    const feedsCard = document.createElement('div');
-    feedsCard.classList.add('card', 'border-0');
-    elements.feeds.append(feedsCard);
-    const feedsBody = document.createElement('div');
-    feedsBody.classList.add('card-body');
-    feedsCard.append(feedsBody);
-    const feedsTitle = document.createElement('h2');
-    feedsTitle.classList.add('card-title', 'h4');
-    feedsTitle.textContent = i18n.t('feeds');
-    feedsBody.append(feedsTitle);
-    const ul = document.createElement('ul');
-    ul.classList.add('list-group', 'border-0', 'rounded-0');
-    feedsCard.append(ul);
-    watchedState.feeds.forEach((feed) => {
-      const li = document.createElement('li');
-      li.classList.add('list-group-item', 'border-0', 'border-end-0');
-      ul.append(li);
-      const h3 = document.createElement('h3');
-      h3.classList.add('h6', 'm-0');
-      h3.textContent = feed.text;
-      const p = document.createElement('p');
-      p.classList.add('m-0', 'small', 'text-black-50');
-      p.textContent = feed.description;
-      li.append(h3, p);
-    });
-  };
-
-  const renderPosts = (elements, i18n, watchedState) => {
-    elements.posts.innerHTML = '';
-    const postsCard = document.createElement('div');
-    postsCard.classList.add('card', 'border-0');
-    elements.posts.append(postsCard);
-    const postsBody = document.createElement('div');
-    postsBody.classList.add('card-body');
-    postsCard.append(postsBody);
-    const postsTitle = document.createElement('h2');
-    postsTitle.classList.add('card-title', 'h4');
-    postsTitle.textContent = i18n.t('posts');
-    postsBody.append(postsTitle);
-    const ul = document.createElement('ul');
-    ul.classList.add('list-group', 'border-0', 'rounded-0');
-    postsCard.append(ul);
-
-    watchedState.posts.forEach((post) => {
-      const {
-        id, text, link,
-      } = post;
-      const li = document.createElement('li');
-      const button = document.createElement('button');
-      const a = document.createElement('a');
-
-      li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
-      ul.prepend(li);
-
-      a.href = link;
-      a.textContent = text;
-      a.target = '_blank';
-      a.setAttribute('rel', 'noopener noreferrer');
-      a.setAttribute('data-post-id', id);
-      if (watchedState.ulStateOpened.includes(id)) {
-        a.classList.add('fw-normal');
-      } else {
-        a.classList.add('fw-bold');
-      }
-
-      button.type = 'button';
-      button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-      button.textContent = i18n.t('button');
-      button.setAttribute('data-post-id', id);
-      li.append(a, button);
-    });
-
-    ul.addEventListener('click', (e) => {
-      const button = e.target.closest('button');
-      const a = e.target.closest('a');
-
-      if (button) {
-        watchedState.ulStateOpened.push(button.dataset.postId);
-        const [targetPost] = watchedState.posts.filter((post) => post.id === button.dataset.postId);
-        const targetA = document.querySelector(`a[data-post-id="${button.dataset.postId}"]`);
-        targetA.classList.remove('fw-bold');
-        targetA.classList.add('fw-normal');
-
-        const modal = new Modal(elements.modal);
-        elements.modalTitle.textContent = targetPost.text;
-        elements.modalBody.textContent = targetPost.description;
-        elements.modalArticle.href = targetPost.link;
-        modal.show();
-      } else if (a) {
-        watchedState.ulStateOpened.push(a.dataset.postId);
-        a.classList.remove('fw-bold');
-        a.classList.add('fw-normal');
-      }
-    });
   };
 
   const watchedState = onChange(state, (path) => {
