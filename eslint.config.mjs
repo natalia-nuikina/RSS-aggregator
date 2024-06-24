72b64b89a6f8df86fdc926db3aa794f5baa6dd21
Наталья Нуйкина <me@nuikina.ru>
2024-06-24 16:07:32 +0300

    Проверка 1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,31 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const compat = new FlatCompat({
+  baseDirectory: dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
-export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-];
+export default [...compat.extends('airbnb-base'), {
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+    },
+
+    ecmaVersion: 'latest',
+    sourceType: 'script',
+  },
+
+  rules: {
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'no-param-reassign': 0,
+    'no-console': 0,
+  },
+}];
